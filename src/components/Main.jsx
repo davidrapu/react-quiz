740fcd1d2cccfc9f09907479e71b9d03783d814d
davidrapu <168489960+davidrapu@users.noreply.github.com>
2025-09-02 22:02:17 +0100

    lifted state, and using state status to choose what stage to show

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,83 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import Button from "./Button";
 import Quiz from "./quiz/Quiz";
+import questionList from "../assets/data/questions.json";
 
+
+const initialState = {
+  num: 0,
+  userPoints: 0,
+  selectedId: null,
+  prevSelectedId: null,
+  highScore: Number(localStorage.getItem('highScore')) || 0,
+  // Intro | Start | Finish
+  status: 'intro'
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "Next":
+      return {
+        ...state,
+        num: state.num + 1,
+        prevSelectedId: state.selectedId,
+        selectedId: null,
+      };
+    case "Prev":
+      return { ...state, num: state.num - 1, selectedId: state.prevSelectedId };
+    case "setSelectedId":
+      return { ...state, selectedId: action.payload };
+    case "increaseUserPoints":
+      return { ...state, userPoints: state.userPoints + action.payload };
+    case "setStatus":
+      return {...state, status: action.payload}
+    case 'setHighscore':
+      localStorage.setItem('highScore', action.payload);
+      return { ...state, highScore: action.payload };
+    case "reset":
+      return {...initialState, highScore: state.highScore}
+    default:
+      return state;
+  }
+}
+const totalPoints = questionList.questions.reduce((acc, qObj) => acc + qObj.points, 0)
 export default function Main() {
-  const [hasStarted, setHasStarted] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState)
   function handleQuizStart() {
-    setHasStarted(true);
+    dispatch({type: 'setStatus', payload: 'start'})
+  }
+  function handleReset(){
+    dispatch({type: 'reset'})
   }
   return (
     <main>
-      {!hasStarted ? (
+      {state.status === "intro" && (
         <div className="intro">
           <h2>Welcome to the React Quiz</h2>
           <p>15 questions to test your React mastery</p>
           <Button onClick={handleQuizStart}>Let's start!</Button>
         </div>
-      ) : (
-        <Quiz />
+      )}
+      {state.status === "start" && (
+        <Quiz
+          state={state}
+          dispatch={dispatch}
+          questions={questionList.questions}
+        />
+      )}
+      {state.status === "finish" && (
+        <div className="result-container">
+          <div className="result">
+            <p id="score">
+              You scored {state.userPoints} out of{" "}
+              {totalPoints} (
+              {((state.userPoints / totalPoints) * 100).toFixed(2)}%)
+            </p>
+            <p id="highscore">(Highscore: {state.highScore} points)</p>
+          </div>
+          <div className="reset">
+            <Button onClick={handleReset}>Reset</Button>
+          </div>
+        </div>
       )}
     </main>
   );
