a9b1d354eda69b8b2bfcedebc5822561c3b4892f
davidrapu <168489960+davidrapu@users.noreply.github.com>
2025-09-02 17:40:28 +0100

    using useReducer to store all related states, now user can fully answer the quiz, all thats left is resetting the quiz

--- src/components/quiz/Question.jsx
+++ src/components/quiz/Question.jsx
@@ -1,14 +1,37 @@
 import Button from "../Button";
 
-export default function Question({ questionObj }) {
+export default function Question({ questionObj, state, dispatch }) {
+  function handleClick(e) {
+    dispatch({ type: "setSelectedId", payload: Number(e.target.value) })
+
+    if (questionObj.correctOption === Number(e.target.value)){
+      dispatch({type: 'increaseUserPoints', payload: questionObj.points})
+    }
+  }
   return (
     <div className="question-answer-container">
       <h2> {questionObj.question} </h2>
       <div className="answers">
         {questionObj.options.map((answer, index) => (
-            <Button key={index} className='answer'>
-                {answer}
-            </Button>
+          <Button
+            onClick={handleClick}
+            key={index}
+            value={index}
+            disabled={state.selectedId != null}
+            className={`answer 
+              ${state.selectedId === index ? "selected" : ""} 
+              ${
+                state.selectedId != null
+                  ? questionObj.correctOption === index
+                    ? "correct"
+                    : "wrong"
+                  : ""
+              }
+              ${state.selectedId != null ? 'disabled' : ""}
+              `}
+          >
+            {answer}
+          </Button>
         ))}
       </div>
     </div>
--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -1,36 +1,58 @@
 import questionList from "../../assets/data/questions.json";
-import { useState } from "react";
+import { useReducer } from "react";
 import Question from "./Question";
 import Button from "../Button";
 
 const questions = questionList.questions;
+const initialState = {
+  num : 0,
+  userPoints : 0,
+  selectedId : null,
+  prevSelectedId: null,
+};
+
+function reducer(state, action){
+  switch (action.type) {
+    case 'Next':
+      return {...state, num: state.num + 1,  prevSelectedId: state.selectedId, selectedId: null}
+    case 'Prev':
+      return {...state, num: state.num - 1, selectedId: state.prevSelectedId}
+    case 'setSelectedId':
+      return {...state, selectedId: action.payload}
+    case 'increaseUserPoints':
+      return {...state, userPoints: state.userPoints + action.payload}
+    default:
+      return state
+  }
+}
 export default function Quiz() {
-  const [num, setNum] = useState(1);
-  const [userPoints, setUserPoints] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState)
+  function handleNext(){
+    if (state.num < questions.length -1) dispatch({type : 'Next'})
+  }
 
   return (
     <div className="quiz">
       <div className="progress-bar">
-        <progress value={num} max={15}>
+        <progress value={state.num + 1} max={15}>
           {" "}
         </progress>
         <p>
           <span>
-            Question <strong>{num}</strong> / {questions.length}
+            Question <strong>{state.num + 1}</strong> / {questions.length}
           </span>
           <span>
-            <strong>{userPoints}</strong> / {questions.reduce((acc, qObj) => acc + qObj.points, 0)} points
+            <strong>{state.userPoints}</strong> /{" "}
+            {questions.reduce((acc, qObj) => acc + qObj.points, 0)} points
           </span>
         </p>
       </div>
-      <Question questionObj={questions[3]} />
+      <Question questionObj={questions[state.num]} state={state} dispatch={dispatch} />
       <div className="timer-next-container">
-        <p>
-            17:30
-        </p>
-        <Button>
-            Next
-        </Button>
+        <p>17:30</p>
+        <div className="buttons">
+          <Button onClick={handleNext}>Next ▶</Button>
+        </div>
       </div>
     </div>
   );
